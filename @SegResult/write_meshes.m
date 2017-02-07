174b4355fa90cf992cf956668f997819c20158a6
Julie Winchester <julia.m.winchester@gmail.com>
2017-02-07 16:42:24 -0500

    Fleshing out @SegResult methods

--- @SegResult/write_meshes.m
+++ @SegResult/write_meshes.m
@@ -1,4 +1,12 @@
 function write_meshes(SegResult, dirPath)
 % WRITE_MESHES - Save mesh files
+	
+	touch(dirPath);
 
+	for i = 1:length(SegResult.mesh)
+		disp(['Saving mesh ' SegResult.mesh{i}.Aux.name ' as OFF file...');
+		write_off(fullfile(dirPath, [SegResult.mesh{i}.Aux.name '.off']), ... 
+			segResult.mesh{i}.V, segResult.mesh{i}.F);
+	end
+	
 end
