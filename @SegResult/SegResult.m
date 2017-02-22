233add752628507de12e0ca7de46d2d776389c33
Julia Winchester <Julia.Winchester@stonybrook.edu>
2017-02-22 10:24:24 -0500

    Segment meshes are now @Mesh objects, write methods for @Mesh are now used

--- @SegResult/SegResult.m
+++ @SegResult/SegResult.m
@@ -1,6 +1,25 @@
 classdef SegResult < handle
-% Class storing results from consistent spectral clustering of surface regions
-% and methods to export these results
+% SEGRESULT - Class storing results from surface region spectral clustering
+% This class stores surface meshes, and generates and stores mesh surface region 
+% segments given a concatenated column vector of segment ID values (1:n) per 
+% vertex for all surface meshes. Methods calculate secondary data and export 
+% surface meshes and secondary data. 
+%
+% Constructor syntax: obj = SegResult(flatSamples, kIdx, vIdxCumSum, cfg)
+% 
+% Constructor inputs: 
+%	flatSamples -  
+%	kIdx        -
+%	vIdxCumSum  -
+%	cfg         -
+% 
+% Attributes:
+%	cfg  -
+%	data -
+%	mesh -
+%	R    -
+
+
 
 	properties
 	% Documentation needed
@@ -25,6 +44,7 @@ classdef SegResult < handle
 			    V = obj.mesh{i}.V';
 			    F = obj.mesh{i}.F';
 			    for j = 1:size(obj.mesh{i}.segment, 1)
+			    	obj.mesh{i}.segment{j} = Mesh();
 			        obj.mesh{i}.segment{j}.V = V(segIdx == j, :)';
 			        vIdx = 1:length(V);
 			        segVertIdxOrig = vIdx(segIdx == j);
