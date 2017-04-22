33bb45cf86abfb1125b87d041406484c82532ef5
Julia Winchester <Julia.Winchester@stonybrook.edu>
2017-04-22 16:31:17 -0400

    Updated spark_on_sge to 0.2

--- cpd/cluster_cpd_mst.m
+++ cpd/cluster_cpd_mst.m
@@ -1,4 +1,4 @@
 function cluster_cpd_mst(cfgPath)
 % CLUSTER_CPD_MST - Launch Spark environment/application for CPD MST step
-	eval(['../util/spark_on_sge-0.1/spark_run.sh ./cpd_mst/cpd_mst.py ' cfgPath]);
+	eval(['../util/spark_on_sge-0.2/spark_run.sh ./cpd_mst/cpd_mst.py ' cfgPath]);
 end
